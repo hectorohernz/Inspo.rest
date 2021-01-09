216a36b4eb3ace8679e04693dc8689c75eb2cd34
highsgod <hectorohernz@gmail.com>
2021-01-09 17:05:08 -0500

    saving

--- packages/server/routes/rootUser.js
+++ packages/server/routes/rootUser.js
@@ -5,10 +5,7 @@ const passwordHashing = require("../../utils/bcrypt/passwordHashing");
 const vaildateUserInfomation = require("../../utils/vaildation/userVaildation");
 const generateAccessToken = require("../../utils/authencation/generateAccessToken");
 const authencationToken = require("../../utils/authencation/authToken");
-
-
-
-
+const compareHashedPassword = require("../../utils/bcrypt/compareHashedPassword");
  const findByUsername = async (username) => {
   let user =  await User.findOne({ username }).exec();
   return user;
@@ -20,22 +17,41 @@ const authencationToken = require("../../utils/authencation/authToken");
  }
 
 
-router.get("/", authencationToken, async (req, res) => {
+ router.get("/", authencationToken, async (req, res) => {
   const isUserInDatabase = await findByUsername(req.username);
-
   if(!isUserInDatabase){
-    logger.info(isUserInDatabase);
-    res.send("Not Found In Database");
+    res.send("User Not Found In Database");
     return;
   }
-  const {password} = isUserInDatabase;
-
-  // if password is correct return info
-  // else
-  // else return not exist
-  res.send("Api Workings");
+  res.json(isUserInDatabase);
 });
 
+
+router.post("/login", async (req,res) => {
+  const {username,password} = req.body;
+  const user = await findByUsername(username);
+  const responseMessage = {};
+
+  if(!user){
+    responseMessage.message = "User Doesn't Exist";
+    responseMessage.token = null;
+    responseMessage.success = false;
+    return res.json(responseMessage);
+  }
+  
+  let isPasswordTheSameAsHashed = await compareHashedPassword(password,user.password);
+
+  if(!isPasswordTheSameAsHashed){
+      responseMessage.message = "Password don't match!";
+      responseMessage.token = null;
+      responseMessage.success = false;
+      return res.json(responseMessage);
+  }
+  responseMessage.message = "Token Granted";
+  responseMessage.token = generateAccessToken(username);
+  responseMessage.success = true;
+  return res.json(responseMessage);
+})
 
 async function getIsEmailAndUsernameAvailable(user){
   let {emailAddress, username} = user;
@@ -61,11 +77,7 @@ async function getIsEmailAndUsernameAvailable(user){
 router.post("/", async (req, res) => {
   let user = {firstName, lastName, username, password, emailAddress, phoneNumber} = req.body;
   try {
-    user.firstName = user.firstName.trim();
-    user.lastName = user.lastName.trim();
-    user.username = user.username.trim();
     let isUserInfomationVaild = vaildateUserInfomation(user);
-
     if(isUserInfomationVaild[0]){
         let isEmailAndUsernameAvailable = await getIsEmailAndUsernameAvailable(user);
         if(!isEmailAndUsernameAvailable.status){
@@ -86,5 +98,46 @@ router.post("/", async (req, res) => {
     res.send(err).status(501);
   }
 });
+
+router.put("/", authencationToken, async (req,res) => {
+  let user = {firstName, lastName, username, password, emailAddress, phoneNumber} = req.body;
+  try{
+    let isUserInfomationVaild = vaildateUserInfomation(user);
+    if(isUserInfomationVaild[0]){
+      let isEmailAndUsernameAvailable = await getIsEmailAndUsernameAvailable(user);
+      
+      if(!isEmailAndUsernameAvailable.status){
+        res.send(isEmailAndUsernameAvailable.message);
+        return;
+      }
+
+      user.password = await passwordHashing(password); 
+      let token = generateAccessToken({username:user.username});
+      res.json(token).status(201);
+      return;
+    
+    }else{
+      res.send(isUserInfomationVaild[1]).status(201);
+      return;
+    }
+  }catch(err){
+    logger.error(err);
+    res.send(err).status(501);
+    return;
+  }
+});
+
+
+router.delete("/", authencationToken, async (req,res) => {
+  let user = {firstName, lastName, username, password, emailAddress, phoneNumber} = req.body;
+  try {
+    let isUserDeleted = await User.deleteOne({username: user.username});
+    return res.send(isUserDeleted); 
+  } catch (err) {
+    logger.error(err);
+    res.send(err).status(501);
+    return;
+  }
+})
 
 module.exports = router;
